1ea9b25fd04d2df1934bae732b5e827cc6dc7623
Przemek <18726317+ppietrasik@users.noreply.github.com>
2018-11-14 15:03:15 +0100

    correcting unnecessary empty string hash merge

--- 14-11-2018/przemek/rot13/rot13.rb
+++ 14-11-2018/przemek/rot13/rot13.rb
@@ -1,7 +1,7 @@
 class String
   alphabet_downcase = ('a'..'z').to_a.zip(('a'..'z').to_a.rotate(13)).to_h
   alphabet_uppercase = ('A'..'Z').to_a.zip(('A'..'Z').to_a.rotate(13)).to_h
-  ALPHABET = alphabet_downcase.merge(alphabet_uppercase.merge({' ' => ' ', '' => ''}))
+  ALPHABET = alphabet_downcase.merge(alphabet_uppercase.merge({' ' => ' '}))
 
   def rot13
     self.chars.map do |char| 
