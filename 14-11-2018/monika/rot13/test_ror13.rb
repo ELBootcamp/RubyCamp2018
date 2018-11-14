37e78f69a4dac5eca8f91ded7d1a5fc28bcaa9b8
elpassion <elpassion@elpassions-MacBook-Pro-3.local>
2018-11-14 14:17:45 +0100

    add spec to algorithm ror13

--- 14-11-2018/monika/rot13/test_ror13.rb
+++ 14-11-2018/monika/rot13/test_ror13.rb
@@ -3,16 +3,35 @@ require 'test/unit'
 
 class TestRor13 < Test::Unit::TestCase
 
-  def test_change
+  def test_change1
     assert_equal "uryyb", "hello".ror13 
-    assert_equal "urwany hejnal", "hejnal urwany".ror13
-    assert_equal "tekst", "tekst".ror13.ror13
-    assert_equal "", "".ror13   
-    assert_equal nil, nil.ror13
-    assert_equal "uRYyb", "hELlo".ror13   
-    assert_equal "a", "a".ror13.ror13
-    assert_raise ArgumentError {"ą".ror13}   
-    assert_raise ArgumentError {"1".ror13}   
+  end
 
+  def test_change2
+    assert_equal "urwany hejnal", "hejnal urwany".ror13
+  end
+
+  def test_change3
+    assert_equal "tekst", "tekst".ror13.ror13
+  end
+
+  def test_change4
+    assert_equal "", "".ror13  
+  end
+
+  def test_change5 
+    assert_equal "uRYyb", "hELlo".ror13 
+  end
+
+  def test_change6  
+    assert_equal "a", "a".ror13.ror13
+  end
+
+  def test_change7
+    assert_raise(ArgumentError.new("polish letters not allowed")) {"ą".ror13} 
+  end
+
+  def test_change8  
+    assert_raise(ArgumentError.new("numbers not allowed")) {"1".ror13}   
   end 
 end
