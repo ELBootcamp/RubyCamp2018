cf7725973c31dbf61f513986cea7e983e3770b57
Przemek <18726317+ppietrasik@users.noreply.github.com>
2018-11-15 13:04:31 +0100

    refactoring for nil case

--- 15-11-2018/greeting_task/greet.rb
+++ 15-11-2018/greeting_task/greet.rb
@@ -1,6 +1,8 @@
 class Greet
-  def greet(name = "my friend")
-    if name.is_a? Array 
+  def greet(name)
+    name ||= "my friend"
+    
+    if name.is_a?(Array) 
       if name.length == 2 
         if name.any? {|name| name.include?("\"")}  
           name = name.map do |name|
@@ -20,19 +22,15 @@ class Greet
       elsif name.any? {|name| name == name.upcase}
         upcase_name = name.detect {|name| name == name.upcase }
         name.delete_at(name.index(upcase_name))
-        "Hello, #{name.first} and #{name[-1]}. AND HELLO #{upcase_name}!"
+        "Hello, #{name.first} and #{name.last}. AND HELLO #{upcase_name}!"
       else  
         last_name = name.last
         name.delete_at(name.length-1)
         "Hello, " + name.join(", ") + ", and " + last_name + "."
       end
-    elsif
-      name && name == name.upcase
-      "HELLO #{name}."
-    elsif name 
-      "Hello, Bob."
-    else
-      "Hello, my friend."
-   end
+    else name == name.upcase
+      name == name.upcase ? "HELLO #{name}." : "Hello, #{name}."
+    end
+
   end
 end
