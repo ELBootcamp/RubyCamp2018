fdc62436ed5b453a9d696c57e776e511d338634d
elpassion <elpassion@elpassions-MacBook-Pro-3.local>
2018-11-15 14:09:57 +0100

    some refactoring applied

--- 15-11-2018/greeting_task/greet.rb
+++ 15-11-2018/greeting_task/greet.rb
@@ -2,19 +2,19 @@ class Greet
   def greet(name)
     if name.is_a? Array 
       if name.any? {|name| name.include?("\"")}  
-          name = name.map do |name|
-            name.include?("\"") ? name.tr("\"","") : name
-          end        
-          last_name = name.last
-          name.delete_at(name.length-1) 
-          "Hello, #{name.join(", ")} and #{last_name}."
+        name = name.map do |name|
+          name.include?("\"") ? name.tr("\"","") : name
+        end        
+        last_name = name.last
+        name.delete_at(name.length-1) 
+        "Hello, #{name.join(", ")} and #{last_name}."
       elsif name.any? {|name| name.include?(",")}
-          name = name.map do |name|
-            name.include?(",") ? name.split(", ") : name
-          end.flatten
-          last_name = name.last
-          name.delete_at(name.length-1) 
-          "Hello, #{name.join(", ")}, and #{last_name}."      
+        name = name.map do |name|
+        name.include?(",") ? name.split(", ") : name
+        end.flatten
+        last_name = name.last
+        name.delete_at(name.length-1) 
+        "Hello, #{name.join(", ")}, and #{last_name}."      
       elsif name.any? {|name| name == name.upcase}
         upcase_name = name.detect {|name| name == name.upcase }
         name.delete_at(name.index(upcase_name))
