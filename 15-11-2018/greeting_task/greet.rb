c892efd348fe0d80361ede5c8f9f122f8fafac12
elpassion <elpassion@elpassions-MacBook-Pro-3.local>
2018-11-15 13:50:09 +0100

    some refactoring applied

--- 15-11-2018/greeting_task/greet.rb
+++ 15-11-2018/greeting_task/greet.rb
@@ -1,12 +1,10 @@
 class Greet
-  def greet(name = "my friend")
+  def greet(name)
     if name.is_a? Array 
-      #if name.length == 2 
       if name.any? {|name| name.include?("\"")}  
           name = name.map do |name|
             name.include?("\"") ? name.tr("\"","") : name
           end        
-          #"Hello, #{name.first} and #{name.last}."
           last_name = name.last
           name.delete_at(name.length-1) 
           "Hello, #{name.join(", ")} and #{last_name}."
@@ -22,24 +20,15 @@ class Greet
         name.delete_at(name.index(upcase_name))
         "Hello, #{name.first} and #{name[-1]}. AND HELLO #{upcase_name}!"
       else  
-      #elsif 
-        #"Hello, #{name.first} and #{name.last}."
         last_name = name.last
         name.delete_at(name.length-1)
         names= name.join(", ")
         puts names
         "Hello, "+names+"#{name.length > 1? ",": ""} and " + last_name + "."  
-        #if name.length == 1
-         # "Hello, " + names + " and " + last_name + "."         
-        #else
-         # "Hello, " + names + ", and " + last_name + "."
-        #end
       end
-    elsif
-      name && name == name.upcase
-      "HELLO #{name}."
     elsif name 
-      "Hello, Bob."
+      "Hello, #{name}."
+      "#{name == name.upcase ? "HELLO ": "Hello, "}"+ name + "."
     else
       "Hello, my friend."
    end
