2916f629ee6d06c1f9eec23bc52e703389ddfad9
elpassion <elpassion@elpassions-MacBook-Pro-3.local>
2018-11-15 12:39:20 +0100

    Add greeting task

---
+++ 15-11-2018/greeting_task/greet.rb
@@ -0,0 +1,38 @@
+class Greet
+  def greet(name = "my friend")
+    if name.is_a? Array 
+      if name.length == 2 
+        if name.any? {|name| name.include?("\"")}  
+          name = name.map do |name|
+            name.include?("\"") ? name.tr("\"","") : name
+          end        
+          "Hello, #{name.first} and #{name.last}."
+        elsif name.any? {|name| name.include?(",")}
+          name = name.map do |name|
+            name.include?(",") ? name.split(", ") : name
+          end.flatten
+          last_name = name.last
+          name.delete_at(name.length-1) 
+          "Hello, #{name.join(", ")}, and #{last_name}."
+        else 
+          "Hello, #{name.first} and #{name.last}."
+        end
+      elsif name.any? {|name| name == name.upcase}
+        upcase_name = name.detect {|name| name == name.upcase }
+        name.delete_at(name.index(upcase_name))
+        "Hello, #{name.first} and #{name[-1]}. AND HELLO #{upcase_name}!"
+      else  
+        last_name = name.last
+        name.delete_at(name.length-1)
+        "Hello, " + name.join(", ") + ", and " + last_name + "."
+      end
+    elsif
+      name && name == name.upcase
+      "HELLO #{name}."
+    elsif name 
+      "Hello, Bob."
+    else
+      "Hello, my friend."
+   end
+  end
+end
