cac8c01215f990b58215642ada2a94565dd66c09
Przemek <18726317+ppietrasik@users.noreply.github.com>
2018-11-15 14:28:53 +0100

    refactored code

--- 15-11-2018/greeting_task/greet.rb
+++ 15-11-2018/greeting_task/greet.rb
@@ -3,34 +3,28 @@ class Greet
     name ||= "my friend"
     
     if name.is_a?(Array) 
-      if name.length == 2 
-        if name.any? {|name| name.include?("\"")}  
-          name = name.map do |name|
-            name.include?("\"") ? name.tr("\"","") : name
-          end        
-          "Hello, #{name.first} and #{name.last}."
-        elsif name.any? {|name| name.include?(",")}
-          name = name.map do |name|
-            name.include?(",") ? name.split(", ") : name
-          end.flatten
-          last_name = name.last
-          name.delete_at(name.length-1) 
-          "Hello, #{name.join(", ")}, and #{last_name}."
-        else 
-          "Hello, #{name.first} and #{name.last}."
-        end
+      if name.any? {|name| name.include?("\"")}  
+        name = name.map do |name|
+          name.include?("\"") ? name.tr("\"","") : name
+        end        
+        "Hello, #{name.first} and #{name.last}."
       elsif name.any? {|name| name == name.upcase}
         upcase_name = name.detect {|name| name == name.upcase }
         name.delete_at(name.index(upcase_name))
         "Hello, #{name.first} and #{name.last}. AND HELLO #{upcase_name}!"
-      else  
-        last_name = name.last
-        name.delete_at(name.length-1)
-        "Hello, " + name.join(", ") + ", and " + last_name + "."
+      elsif name.length == 2 && name.none? {|name| name.include?(",")}
+        "Hello, #{name.first} and #{name.last}."
+      else 
+        if name.any? {|name| name.include?(",")}
+          name = name.map do |name|
+            name.include?(",") ? name.split(", ") : name
+          end.flatten
+        end
+        last_name = name.pop
+        "Hello, #{name.join(", ")}, and #{last_name}."
       end
     else name == name.upcase
       name == name.upcase ? "HELLO #{name}." : "Hello, #{name}."
     end
-
   end
 end
