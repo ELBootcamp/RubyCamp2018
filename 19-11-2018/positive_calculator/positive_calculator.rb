d5d377532e2df8ca3a62f01ee5d3d1fb6d8e352d
Grzesiek Wilusz <elpassion@ELs-MacBook-Pro.local>
2018-11-19 11:57:37 +0100

    Support for subtract method

--- 19-11-2018/positive_calculator/positive_calculator.rb
+++ 19-11-2018/positive_calculator/positive_calculator.rb
@@ -25,7 +25,35 @@ class PositiveCalculator
       end
       sum
     end
+  end
 
+  def subtract(string_numbers)
+    if string_numbers.empty?
+      0
+    elsif string_numbers[0..1] == '//'
+      subtract_sum = 0.0
+      string_numbers = string_numbers[2..-1].split('\n')
+      delimiter = string_numbers.first
+      string_numbers = string_numbers.last
+      string_numbers.split(delimiter)[1..-1].each do |element| 
+        raise NegativesNotAllowed if element.to_f.negative?  
+        element = "0" if element.to_f > 1000
+        subtract_sum += element.to_f 
+      end      
+      string_numbers.split(delimiter).first.to_f - subtract_sum
+    else 
+      subtract_sum = 0.0
+      string_numbers = string_numbers.tr('\n', ',')
+      raise NotEnoughNumbers if string_numbers.split(',').length <=1   
+      string_numbers.split(',')[1..-1].each do |element| 
+        raise NegativesNotAllowed if element.to_f.negative?
+        p element.to_f
+        element = "0" if element.to_f > 1000
+        subtract_sum += element.to_f
+      end
+      raise NegativesNotAllowed if string_numbers.split(',').first.to_f.negative?
+      string_numbers.split(',').first.to_f - subtract_sum
+    end
   end
 
 end
--- 19-11-2018/positive_calculator/test_positive_calculator.rb
+++ 19-11-2018/positive_calculator/test_positive_calculator.rb
@@ -9,7 +9,7 @@ class TestPositiveCalculator < Test::Unit::TestCase
 
   def test_add_numbers
     assert_equal 21, 
-    PositiveCalculator.new.add('1,5,5,4,6s')
+    PositiveCalculator.new.add('1,5,5,4,6')
   end
 
   def test_new_line_separator_support
@@ -37,6 +37,39 @@ class TestPositiveCalculator < Test::Unit::TestCase
   def test_big_numbers
     assert_equal 1,
     PositiveCalculator.new.add('1,1001')
+  end
+
+
+  def test_subtract_numbers
+    assert_equal 6, 
+    PositiveCalculator.new.subtract('10,4')
+  end
+
+  def test_new_line_separator_support_subtract_method
+    assert_equal 1, 
+    PositiveCalculator.new.subtract('3\n1\n1')
+  end
+
+  def test_not_enough_numbers_error_subtract_method
+    assert_raise NotEnoughNumbers do
+       PositiveCalculator.new.subtract('1\n')
+    end
+  end
+
+  def test_subtract_method_delimiter
+    assert_equal 1, 
+    PositiveCalculator.new.subtract('//;\n2;1')
+  end
+
+  def test_negatives_subtract_method
+    assert_raise NegativesNotAllowed do
+       PositiveCalculator.new.subtract('-2,1')
+    end
+  end
+
+  def test_big_numbers_subtract_method
+    assert_equal 1,
+    PositiveCalculator.new.subtract('1,1001')
   end
 
 end
