68cf1bed7c6f9dbd86d32fbbff5e232b1777b11a
elpassion <elpassion@elpassions-MacBook-Pro-3.local>
2018-11-19 13:06:35 +0100

    add refactoring

---
+++ 19-11-2018/monika/positive_calculator/positive_calculator.rb
@@ -0,0 +1,40 @@
+class PositiveCalculator
+
+  def add(string_numbers)   
+    raise NegativesNotAllowed if string_numbers.include? "-"
+    return 0 if string_numbers.empty?
+    string_numbers[0..1] == '//' && string_numbers = change_slash_to_comma(string_numbers)
+    string_numbers = string_numbers.tr('\n', ',') 
+    calculate_sum(string_numbers)      
+  end
+
+  def subtract(string_numbers)
+    raise NegativesNotAllowed if string_numbers.include? "-"
+    return 0 if string_numbers.empty?
+    string_numbers[0..1] == '//' && string_numbers = change_slash_to_comma(string_numbers)
+    string_numbers = string_numbers.tr('\n', ',') 
+
+    subtraction_result = string_numbers.split(',').first.to_f - calculate_sum(string_numbers[1..-1]) 
+    raise NegativesNotAllowed if subtraction_result < 0
+    subtraction_result  
+  end
+  
+  def change_slash_to_comma(string_to_change)
+    string_to_change = string_to_change[2..-1].split('\n')
+    delimiter = string_to_change.first
+    string_to_change.last.tr(delimiter, ',')
+  end
+
+  def calculate_sum(numbers)
+    sum = 0.0
+    raise NotEnoughNumbers if numbers.split(',').length <=1   
+    numbers.split(',').each do |element| 
+      element = "0" if element.to_f > 1000
+      sum += element.to_f
+    end
+    sum
+  end
+end
+
+class NotEnoughNumbers < StandardError; end 
+class NegativesNotAllowed < StandardError; end
