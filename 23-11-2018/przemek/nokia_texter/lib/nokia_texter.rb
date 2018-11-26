fd8b32a2a30ff911cdecbc84d376454383747d9b
Przemek <18726317+ppietrasik@users.noreply.github.com>
2018-11-26 15:01:41 +0100

    Changing NokiaTexter#squeeze implementation

--- 23-11-2018/przemek/nokia_texter/lib/nokia_texter.rb
+++ 23-11-2018/przemek/nokia_texter/lib/nokia_texter.rb
@@ -9,19 +9,12 @@ class NokiaTexter
     (raise ArgumentError) unless text_msg.is_a?(String)
     (raise MethodToLong) unless (text_msg.size < 160)
 
-    text_split = squish_split(text_msg)
-    return text_msg if text_split.size <= 1
+    text_split = text_msg.split
+    text_split.size <= 1 && (return text_msg)
 
-    text_split.map!.with_index do |word, index|
-      index % 2 == 0 ? word.capitalize : word.downcase
-    end
-
-    text_split.first.upcase!
-    text_split.join
-  end
-
-  def squish_split(text_input)
-    text_input.split
+    text_split.map.with_index do |word, index|
+      index % 2 == 0 ? word.upcase : word.downcase
+    end.join
   end
 end
 
