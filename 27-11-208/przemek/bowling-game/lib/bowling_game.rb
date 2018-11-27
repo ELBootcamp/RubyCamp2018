6d883928b97059a48a0dfd507f19ee4fc922f658
Przemek <18726317+ppietrasik@users.noreply.github.com>
2018-11-27 16:50:15 +0100

    Add expect to test for BowlingGames that checks multiple frames cases

--- 27-11-208/przemek/bowling-game/lib/bowling_game.rb
+++ 27-11-208/przemek/bowling-game/lib/bowling_game.rb
@@ -6,6 +6,7 @@ class BowlingGame
   end
 
   def roll(pins_num)
+    frames.push([]) if frames.last.size >= 2
     frames.last.push(pins_num)
   end
 end
--- 27-11-208/przemek/bowling-game/spec/bowling_game_spec.rb
+++ 27-11-208/przemek/bowling-game/spec/bowling_game_spec.rb
@@ -7,5 +7,7 @@ RSpec.describe BowlingGame do
     expect(subject.frames.first).to eq([3])
     subject.roll(5)
     expect(subject.frames.first).to eq([3, 5])
+    subject.roll(8)
+    expect(subject.frames).to eq([[3, 5], [8]])
   end
 end
