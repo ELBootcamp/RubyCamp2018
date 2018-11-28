b42a8d304168dc07d1026621d564a8f3135053db
elpassion <elpassion@elpassions-MacBook-Pro-3.local>
2018-11-28 15:17:38 +0100

    Add test and implementation and fix for scoring calculation for tenth round

--- 27-11-208/monika/bowling-game/frame.rb
+++ 27-11-208/monika/bowling-game/frame.rb
@@ -6,12 +6,13 @@ class Frame
     @is_spare = false
     @frame_sum = 0
   end
-  attr_accessor :round, :values, :is_strike, :is_spare, :frame_sum
+  attr_accessor :values, :is_strike, :is_spare, :frame_sum, :frame_sum_next
  
   def check_ten
     if values.reduce(0, :+) >= 10
       values.length == 1 ? @is_strike = true : @is_spare = true
     end
+    @frame_sum_next = values[0..1].reduce(0, :+)
     @frame_sum = values.reduce(0, :+)
   end
 end
--- 27-11-208/monika/bowling-game/game.rb
+++ 27-11-208/monika/bowling-game/game.rb
@@ -13,17 +13,31 @@ class Game
   attr_accessor :round, :counter, :scoring, :frames, :first
 
   def roll(pins)
+    return 'No more rolls allowed' if @round == 10 && @frames[@round].values.length == 3
+    return @frames[@round] if tenth_round_additional_rolls(pins)
+
     if @first && pins != 10
       @counter = pins
       @first = false
     else
-      @round += 1
-      frame = Frame.new
+      @round += 1 if @round != 10
+      frame = Frame.new     
       frame.values = pins == 10 ? [pins] : [@counter, pins]
       frame.check_ten
-      @frames[@round] = frame
-      @first = true
-      frame
+      @frames[@round] = frame 
+      @first = true if @round != 10
+      @frames[@round]
+      
+    end
+  end
+
+  def tenth_round_additional_rolls(pins)
+    if !@frames[10].nil?     
+      @frames[10].values.push(pins)
+      @frames[10].check_ten
+      true
+    else 
+      false
     end
   end
 
@@ -32,10 +46,9 @@ class Game
     @frames.each do |key, value|
       if key != round
         value.frame_sum += frames[key + 1].values[0] if value.is_spare
-        value.frame_sum += frames[key + 1].frame_sum if value.is_strike
+        value.frame_sum += frames[key + 1].frame_sum_next if value.is_strike
       end
       @scoring += value.frame_sum
-      byebug
     end
     @scoring
   end
