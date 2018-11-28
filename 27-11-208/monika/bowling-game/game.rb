622966f560430b2f6fced8f9e982b4740b0a2faa
elpassion <elpassion@elpassions-MacBook-Pro-3.local>
2018-11-28 10:15:58 +0100

    Add test and implementation that returns correct scoring value for two rolls:

--- 27-11-208/monika/bowling-game/game.rb
+++ 27-11-208/monika/bowling-game/game.rb
@@ -5,9 +5,11 @@ class Game
   def initialize
     @round = 0
     @counter = 0
+    @frames = {}
+    @scoring = 0
   end
 
-  attr_accessor :round, :counter
+  attr_accessor :round, :counter, :scoring, :frames
 
   def roll(pins)
     if @counter.zero? && pins != 10
@@ -18,12 +20,17 @@ class Game
       frame.values = pins == 10 ? [pins] : [@counter, pins]
       frame.check_ten
       @counter = 0
-      p frame
+      @frames[@round] = frame
+      frame
     end
   end
 
   def scoring
-    round.zero? && 0
+    round.zero? && (return @scoring)
+    @frames.each do |key, value|
+      value.frame_sum = frame[key+1].values[0] if value.is_spare
+    end
+    @frames[round].frame_sum
   end
 end
 
--- 27-11-208/monika/bowling-game/spec/game_spec.rb
+++ 27-11-208/monika/bowling-game/spec/game_spec.rb
@@ -19,16 +19,25 @@ RSpec.describe Game do
       subject.roll(1)
       expect(subject.roll(4).values).to eq([1,4])
     end 
+    it 'creates four frames and in first frame is strike' do
+      expect(subject.roll(10).values).to eq([10])
+      subject.roll(1)
+      expect(subject.roll(4).values).to eq([1,4])
+      subject.roll(3)
+      expect(subject.roll(5).values).to eq([3,5])
+      subject.roll(7)
+      expect(subject.roll(1).values).to eq([7,1])
+    end 
   end
 
   context "checks scoring calculation" do   
     it 'returns correct scoring value when there was no rolls' do
       expect(subject.scoring).to eq(0)
     end
-    # it 'returns correct scoring value' do
-    #   subject.roll(2)
-    #   subject.roll(2)
-    #   expect(subject.scoring).to eq(4)
-    # end
+    it 'returns correct scoring value for two rolls' do
+       subject.roll(2)
+       subject.roll(2)
+       expect(subject.scoring).to eq(4)
+    end
   end
 end
