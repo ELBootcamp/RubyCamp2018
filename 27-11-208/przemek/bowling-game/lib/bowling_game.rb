c817dabc6e11036337245a8e48dd7cb978eec830
Przemek <18726317+ppietrasik@users.noreply.github.com>
2018-11-28 11:52:19 +0100

    Refactor BogwlingGame with Frame methods strike? and spare?

--- 27-11-208/przemek/bowling-game/lib/bowling_game.rb
+++ 27-11-208/przemek/bowling-game/lib/bowling_game.rb
@@ -10,20 +10,20 @@ class BowlingGame
 
   def roll(*pins_num)
     pins_num.each do |pin|
-      frames.push(Frame.new) if frames.last.values.size >= 2 || frames.last.values.include?(10)
+      frames.push(Frame.new) if frames.last.values.size >= 2 || frames.last.strike?
       frames.last.add_pin_num(pin)
     end
   end
 
   def score
-    frames_arr = self.frames_to_array
-    frames_arr.each.with_index.reduce(0) do |sum, (frame, index)|
-      frame_score = frame.reduce(:+)
-      if(frame_score == 10 && frame.size == 1)
-        frame_score += frames_arr[index + 1].reduce(:+) if frame_score == 10 && frames_arr[index + 1]
-      else
-        frame_score += frames_arr[index + 1].first if frame_score == 10 && frames_arr[index + 1]
+    frames.each.with_index.reduce(0) do |sum, (frame, index)|
+      frame_score = frame.values.reduce(:+)
+      if(frame.strike?)
+        frame_score += self.frames[index + 1].values.reduce(:+)
+      elsif (frame.spare?)
+        frame_score += self.frames[index + 1].values.first
       end
+      
       sum += frame_score
     end
   end
