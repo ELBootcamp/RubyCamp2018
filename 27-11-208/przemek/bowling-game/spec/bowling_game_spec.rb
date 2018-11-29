5e64626070ba01583cc6e534c7ddc2860a9f47f9
Przemek <18726317+ppietrasik@users.noreply.github.com>
2018-11-29 16:25:01 +0100

    Refactor spec for BowlingGame

--- 27-11-208/przemek/bowling-game/spec/bowling_game_spec.rb
+++ 27-11-208/przemek/bowling-game/spec/bowling_game_spec.rb
@@ -12,69 +12,21 @@ RSpec.describe BowlingGame do
   end
 
   it 'checks if frames_to_array method works properly' do
-    subject.roll(5)
-    subject.roll(4)
-    subject.roll(10)
-    subject.roll(3)
-    subject.roll(4)
+    automatize_rolls(subject, [5, 4, 10, 3, 4])
     expect(subject.frames_to_array).to eq([[5, 4], [10], [3, 4]])
   end
 
   it 'check if BowlingGame#score return proper value for 6 rolls' do
-    subject.roll(1)
-    subject.roll(4)
-    subject.roll(4)
-    subject.roll(5)
-    subject.roll(6)
-    subject.roll(4)
-    subject.roll(5)
-    subject.roll(5)
-    subject.roll(10)
-    subject.roll(0)
-    subject.roll(1)
+    automatize_rolls(subject, [1, 4, 4, 5, 6, 4, 5, 5, 10, 0, 1])
     expect(subject.score).to eq(61)
   end
 
   it 'check if BowlingGame#score handles 10th frame correcly' do
     game_10th_spare = BowlingGame.new
-    game_10th_spare.roll(1)
-    game_10th_spare.roll(4)
-    game_10th_spare.roll(4)
-    game_10th_spare.roll(5)
-    game_10th_spare.roll(6)
-    game_10th_spare.roll(4)
-    game_10th_spare.roll(5)
-    game_10th_spare.roll(5)
-    game_10th_spare.roll(10)
-    game_10th_spare.roll(0)
-    game_10th_spare.roll(1)
-    game_10th_spare.roll(7)
-    game_10th_spare.roll(3)
-    game_10th_spare.roll(6)
-    game_10th_spare.roll(4)
-    game_10th_spare.roll(10)
-    game_10th_spare.roll(2)
-    game_10th_spare.roll(8)
-    game_10th_spare.roll(6)
-    
+    automatize_rolls(game_10th_spare, [1, 4, 4, 5, 6, 4, 5, 5, 10, 0, 1, 7, 3, 6, 4, 10, 2, 8, 6])
+
     game_10th_strike = BowlingGame.new
-    game_10th_strike.roll(1)
-    game_10th_strike.roll(4)
-    game_10th_strike.roll(4)
-    game_10th_strike.roll(5)
-    game_10th_strike.roll(6)
-    game_10th_strike.roll(4)
-    game_10th_strike.roll(5)
-    game_10th_strike.roll(5)
-    game_10th_strike.roll(10)
-    game_10th_strike.roll(0)
-    game_10th_strike.roll(1)
-    game_10th_strike.roll(7)
-    game_10th_strike.roll(3)
-    game_10th_strike.roll(6)
-    game_10th_strike.roll(4)
-    game_10th_strike.roll(10)
-    game_10th_strike.roll(10)
+    automatize_rolls(game_10th_strike, [1, 4, 4, 5, 6, 4, 5, 5, 10, 0, 1, 7, 3, 6, 4, 10, 10])
 
     max_game = BowlingGame.new
     12.times do
@@ -86,25 +38,16 @@ RSpec.describe BowlingGame do
   end
 
   it 'raises an ArgumentError if 10th frame contains more than 3 values' do
-    subject.roll(1)
-    subject.roll(4)
-    subject.roll(4)
-    subject.roll(5)
-    subject.roll(6)
-    subject.roll(4)
-    subject.roll(5)
-    subject.roll(5)
-    subject.roll(10)
-    subject.roll(0)
-    subject.roll(1)
-    subject.roll(7)
-    subject.roll(3)
-    subject.roll(6)
-    subject.roll(4)
-    subject.roll(10)
-    subject.roll(2)
-    subject.roll(8)
-    subject.roll(6)
+    automatize_rolls(subject, [1, 4, 4, 5, 6, 4, 5, 5, 10, 0, 1, 7, 3, 6, 4, 10, 2, 8, 6])
     expect { subject.roll(2) }.to raise_error(ArgumentError)
   end
+end
+
+private
+
+def automatize_rolls(game, rolls)
+  rolls.each do |pin|
+    game.roll(pin)
+  end
+  game
 end
