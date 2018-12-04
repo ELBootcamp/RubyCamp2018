dd41933aaa561899d0080be4ffcbec94041dbad7
Przemek <18726317+ppietrasik@users.noreply.github.com>
2018-12-04 10:09:12 +0100

    Add mocks for WeatherFetcher in app tests

--- 3-12-2018/przemek/sinatra_weather/app.rb
+++ 3-12-2018/przemek/sinatra_weather/app.rb
@@ -3,7 +3,18 @@ require "sinatra/reloader" if development?
 require_relative 'lib/weather_fetcher'
 
 get '/' do
-  city_key = WeatherFetcher.get_location_key(params[:city_name])
-  @weather_data = WeatherFetcher.get_5day_forecast(city_key)
+  @weather_data = []
+  if params[:city_name]
+    begin
+      city_key = WeatherFetcher.get_location_key(params[:city_name])
+      @weather_data = WeatherFetcher.get_5day_forecast(city_key)
+    rescue ArgumentError
+      @error_msg = 'Uncorrect city name was given!'
+      @weather_data = []
+    rescue UnknownError
+      @error_msg = "Unknow API error"
+      @weather_data = []
+    end
+  end
   erb :index
 end
--- 3-12-2018/przemek/sinatra_weather/lib/weather_fetcher.rb
+++ 3-12-2018/przemek/sinatra_weather/lib/weather_fetcher.rb
@@ -4,19 +4,34 @@ module WeatherFetcher
   module_function
 
   HOST = "http://dataservice.accuweather.com"
-  API_KEY = "BxGKx35AfPB4AWQotmgLqZGgMZyGgOx5"
+  API_KEY = "8iGFfGHFtGsNzZTCyfwe1BoK9JLGlFwf"
 
   def get_location_key(city_name)
+    (raise ArgumentError) unless city_name
+
     response = HTTP.get("#{HOST}/locations/v1/cities/PL/search?apikey=#{API_KEY}&q=#{city_name}")
     parsed_response = JSON.parse(response.body)
-    parsed_response[0]['Key']
+    parsed_response[0]['Key'] if parsed_response[0]
   end
 
   def get_5day_forecast(city_key)
+    (raise ArgumentError) unless city_key
+
     response = HTTP.get("#{HOST}/forecasts/v1/daily/5day/#{city_key}?apikey=#{API_KEY}&details=true&metric=true")
     parsed_response = JSON.parse(response.body)
     
-    daily_forecasts = parsed_response['DailyForecasts']
-    daily_forecasts
+    (raise UnknownError) unless parsed_response['DailyForecasts']
+
+    weather_data = []
+    parsed_response['DailyForecasts'].each do |day|
+      weather_data << {
+        'Min_temperature' => day['Temperature']['Minimum']['Value'],
+        'Max_temperature' => day['Temperature']['Maximum']['Value'],
+        'Rain_fall' => day['Day']['Rain']['Value']
+      }
+    end
+    weather_data
   end
 end
+
+class UnknownError < StandardError; end
