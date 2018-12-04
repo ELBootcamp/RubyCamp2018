28c4b12fdd4802a12310277bca318037955b7df2
Przemek <18726317+ppietrasik@users.noreply.github.com>
2018-12-04 12:59:25 +0100

    Add support for  WeatherFetcher to sanitize polish characters

--- 3-12-2018/przemek/sinatra_weather/lib/weather_fetcher.rb
+++ 3-12-2018/przemek/sinatra_weather/lib/weather_fetcher.rb
@@ -9,32 +9,34 @@ module WeatherFetcher
   def get_location_key(city_name)
     (raise ArgumentError) unless city_name
 
-    city_name = city_name.tr(' ', '-')
+    city_name = sanitize_polish_characters(city_name.tr(' ', '-'))
 
     response = HTTP.get("#{HOST}/locations/v1/cities/PL/search?apikey=#{API_KEY}&q=#{city_name}")
     parsed_response = JSON.parse(response.body)
-    
+
     parsed_response[0]['Key'] if parsed_response[0]
   end
 
   def get_5day_forecast(city_key)
     (raise ArgumentError) unless city_key
 
-    response = HTTP.get("#{HOST}/forecasts/v1/daily/5day/#{city_key}?apikey=#{API_KEY}&details=true&metric=true")
+    response = HTTP.get("#{HOST}/forecasts/v1/daily/5day/#{city_key}?apikey=#{API_KEY}&language=pl-pl&details=true&metric=true")
     parsed_response = JSON.parse(response.body)
     
     (raise UnknownError) unless parsed_response['DailyForecasts']
 
-    weather_data = []
-    parsed_response['DailyForecasts'].each do |day|
-      weather_data << {
+    parsed_response['DailyForecasts'].map do |day|
+      {
         'Min_temperature' => day['Temperature']['Minimum']['Value'],
         'Max_temperature' => day['Temperature']['Maximum']['Value'],
         'Rain_fall' => day['Day']['Rain']['Value']
       }
     end
-    weather_data
   end
+
+  def sanitize_polish_characters(str)
+    str.tr('ąćęłńóśźż', 'acelnoszz')
+  end  
 end
 
 class UnknownError < StandardError; end
