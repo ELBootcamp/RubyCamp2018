34400021ac2d8d17a7732ca925e754f3bf6f245a
Przemek <18726317+ppietrasik@users.noreply.github.com>
2018-12-04 09:28:55 +0100

    Add test for WeatherFetcher

--- 3-12-2018/przemek/sinatra_weather/spec/weather_fetcher_spec.rb
+++ 3-12-2018/przemek/sinatra_weather/spec/weather_fetcher_spec.rb
@@ -7,7 +7,7 @@ RSpec.describe WeatherFetcher do
     expect(subject.get_location_key('warsaw')).to eq('274663')
   end
 
-  it 'returns proper location key' do
-    expect(subject.get_5day_forecast('274663')).to eq()
+  it 'returns 5day forecast for location key' do
+    expect(subject.get_5day_forecast('274663').size).to eq(5)
   end
 end
