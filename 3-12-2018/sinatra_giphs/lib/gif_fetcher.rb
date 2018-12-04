f05d410060aae03add4aeb528dce97657e53e0dd
elpassion <elpassion@elpassions-MacBook-Pro-3.local>
2018-12-04 09:11:57 +0100

    [WIP] Partially created api

--- 3-12-2018/sinatra_giphs/lib/gif_fetcher.rb
+++ 3-12-2018/sinatra_giphs/lib/gif_fetcher.rb
@@ -6,7 +6,6 @@ module GifFetcher
 
   def gif_url(category)
     response = HTTP.get("http://api.giphy.com/v1/gifs/search?q=#{category}&api_key=dc6zaTOxFJmzC")
-
     parsed_response = JSON.parse(response.body)
 
     parsed_response['data'][0]['images']['fixed_height']['url']
