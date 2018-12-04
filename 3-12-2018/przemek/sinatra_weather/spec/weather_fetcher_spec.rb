e364f56f5b02b5eae9b658fc7745036013f6e727
Przemek <18726317+ppietrasik@users.noreply.github.com>
2018-12-04 15:19:33 +0100

    Add missing test for csv generation

--- 3-12-2018/przemek/sinatra_weather/spec/weather_fetcher_spec.rb
+++ 3-12-2018/przemek/sinatra_weather/spec/weather_fetcher_spec.rb
@@ -14,4 +14,16 @@ RSpec.describe WeatherFetcher do
   it 'returns proper location key for city names containing spaces' do
     expect(subject.get_location_key('nowa huta')).to eq('274456')
   end
+
+  it 'will generate proper csv file for given weather_data' do 
+    weather_data = [
+      {
+        'Min_temperature' => 5,
+        'Max_temperature' => 10,
+        'Rain_fall' => 6
+      }
+    ]
+    csv_string = "Min_temperature,Max_temperature,Rain_fall\n5,10,6\n"
+    expect(subject.weather_data_csv_export(weather_data)).to eq(csv_string)
+  end
 end
