88bc10aed0dffe7b36cc86062f5d982fb8f59526
Grzesiek Wilusz <elpassion@ELs-MacBook-Pro.local>
2018-12-05 16:56:51 +0100

    Add set_user method and before_action

--- 5-12-2018/grzesiek/grzesiek-sample/app/controllers/users_controller.rb
+++ 5-12-2018/grzesiek/grzesiek-sample/app/controllers/users_controller.rb
@@ -1,10 +1,11 @@
 class UsersController < ApplicationController
+  before_action :set_user, only: [:show, :edit, :update, :destroy]
+
   def index
     @users = User.all
   end
 
   def show
-    @user = User.find(params[:id])
   end
 
   def new
@@ -17,17 +18,14 @@ class UsersController < ApplicationController
     if @user.save
       redirect_to @user
     else
-      render 'new'
+      render :new
     end
   end
 
   def edit
-    @user = User.find(params[:id])
   end
 
   def update
-    @user = User.find(params[:id])
-
     if @user.update(user_params)
       redirect_to @user
     else
@@ -36,15 +34,18 @@ class UsersController < ApplicationController
   end
 
   def destroy
-    @user = User.find(params[:id])
+    # user = User.find(params[:id]) # @ tylko wtedy, gdy chcę przechwycić na froncie
     @user.destroy
 
     redirect_to users_path
   end
   
   private
+  def set_user
+    @user = User.find(params[:id])
+  end
 
   def user_params
-    params.require(:user).permit(:name, :last_name, :age)
+    params.require(:user).permit(:name, :last_name, :age) # require(:model)
   end
 end
