56fe054bc159f1a7af20acc623d71596d6fd4620
Przemek <18726317+ppietrasik@users.noreply.github.com>
2018-12-05 14:32:28 +0100

    Add support for UserController to create new user

--- 5-12-2018/przemek/przemek-sample/app/controllers/users_controller.rb
+++ 5-12-2018/przemek/przemek-sample/app/controllers/users_controller.rb
@@ -6,4 +6,20 @@ class UsersController < ApplicationController
   def show
     @user = User.find(params[:id])
   end
+
+  def new
+    @user = User.new
+  end
+
+  def create
+    @user = User.create(user_params)
+    
+    redirect_to @user
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:name, :last_name, :age)
+  end
 end
