b645ff6b9ce1c08edd8a41c41a8890423e8f42fd
Przemek <18726317+ppietrasik@users.noreply.github.com>
2018-12-05 14:40:05 +0100

    Add support for UserController to update user

--- 5-12-2018/przemek/przemek-sample/app/controllers/users_controller.rb
+++ 5-12-2018/przemek/przemek-sample/app/controllers/users_controller.rb
@@ -13,7 +13,18 @@ class UsersController < ApplicationController
 
   def create
     @user = User.create(user_params)
-    
+
+    redirect_to @user
+  end
+
+  def edit
+    @user = User.find(params[:id])
+  end
+
+  def update
+    @user = User.find(params[:id])
+    @user.update(user_params)
+
     redirect_to @user
   end
 
