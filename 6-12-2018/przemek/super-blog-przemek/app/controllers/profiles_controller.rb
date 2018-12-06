4094f8065a1d1f3634434c0511c3fc67871b3aed
Przemek <18726317+ppietrasik@users.noreply.github.com>
2018-12-06 15:35:28 +0100

    Add /profile route with ProfileController

---
+++ 6-12-2018/przemek/super-blog-przemek/app/controllers/profiles_controller.rb
@@ -0,0 +1,6 @@
+class ProfilesController < ApplicationController
+  
+  def index
+    @posts = current_user.posts
+  end
+end
--- 6-12-2018/przemek/super-blog-przemek/config/routes.rb
+++ 6-12-2018/przemek/super-blog-przemek/config/routes.rb
@@ -3,7 +3,9 @@ Rails.application.routes.draw do
 
   devise_for :users
 
+  get '/profile', to: 'profiles#index'
+
   resources :users do 
-    resources :post
+    resources :posts
   end
 end
