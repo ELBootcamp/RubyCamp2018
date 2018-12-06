119a525a8f642645d728e457486f195d23704b94
Grzesiek Wilusz <elpassion@ELs-MacBook-Pro.local>
2018-12-06 14:57:27 +0100

    Add posts table and posts controller

--- 6-12-2018/grzesiek/super-blog-grzesiek/db/schema.rb
+++ 6-12-2018/grzesiek/super-blog-grzesiek/db/schema.rb
@@ -10,10 +10,19 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_12_06_102952) do
+ActiveRecord::Schema.define(version: 2018_12_06_134823) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "posts", force: :cascade do |t|
+    t.string "title"
+    t.string "content"
+    t.bigint "user_id"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+    t.index ["user_id"], name: "index_posts_on_user_id"
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
@@ -27,4 +36,5 @@ ActiveRecord::Schema.define(version: 2018_12_06_102952) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "posts", "users"
 end
